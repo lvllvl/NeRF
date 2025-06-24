94754de0f0959598c998f357d9f22a0138f5ec8a
lvllvl <24905907+lvllvl@users.noreply.github.com>
2025-06-24 01:54:27 +0000

    fix: clean up utils/ folder

--- utils/visualization_utils.py
+++ utils/visualization_utils.py
@@ -83,3 +83,31 @@ def save_comparison(image1, image2, filepath, title1="Ground Truth", title2="Pre
 
     plt.savefig(filepath)
     plt.close()
+
+def get_camera_matrix( fov, aspect_ratio, near, far ):
+    """
+    Generate a camera projection matrix.
+
+    Parameters:
+    --------
+    fov: float
+        - Field of view of the camera in degrees.
+    aspect_ratio: float
+        - Aspect ratio of the image ( width / height ).
+    near: float
+        - Near clipping plane.
+    far: float
+        - Far clipping plane.
+
+    Returns:
+    --------
+    np.ndarray:
+        - The camera projection matrix.
+    """
+    f = 1.0 / np.tan( np.radians( fov ) / 2.0 )
+
+    return np.array( [[f / aspect_ratio, 0, 0, 0 ],
+                      [0, f, 0, 0],
+                      [0, 0, (far + near) / (near - far), (2 * far * near) / (near - far) ],
+                      [0, 0, -1, 0]
+                      ])
